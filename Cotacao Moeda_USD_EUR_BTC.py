8c33c28f9800348a0bc1f944a3a6429314541446
Lucas Reis <lucaswdex@gmail.com>
2023-03-15 12:42:22 -0300

    Trocado o metodo de interface para o PySimpleGUI

--- Cotacao Moeda_USD_EUR_BTC.py
+++ Cotacao Moeda_USD_EUR_BTC.py
@@ -1,35 +1,33 @@
-import requests
-from tkinter import *
+import PySimpleGUI as sg
+from cotacao import pegar_cotacoes
+
+# Layout
+sg.theme('Reddit')
+
+layout = [
+    [sg.Text('Pegar Cotaçao da Moeda')],
+    [sg.InputText(key='nome_cotacao')],
+    [sg.Button('Pegar Cotação'), sg.Button('Cancelar')],
+    [sg.Text('', key='texto_cotacao')],
+]
+
+janela = sg.Window("Sistema de Cotaçoes USD EUR BTC", layout)
+
+while True:
+    evento, valores = janela.read()
+    if evento == sg.WIN_CLOSED or evento == 'Cancelar':
+        break
+    if evento == 'Pegar Cotação':
+        codigo_cotacao = valores["nome_cotacao"]
+        cotacao = pegar_cotacoes(codigo_cotacao)
+        janela['texto_cotacao'].Update(f"A cotação do {codigo_cotacao} é de R${cotacao}")
 
 
-def pegarCotacoes():
-    cotacao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 
-    cotacao_dic = cotacao.json()
-
-    cotacao_dolar = cotacao_dic['USDBRL']['bid']
-    cotacao_euro = cotacao_dic['EURBRL']['bid']
-    cotacao_btc = cotacao_dic['BTCBRL']['bid']
-
-    texto = f'''
-    Dólar: {cotacao_dolar}
-    Euro: {cotacao_euro}
-    BTC: {cotacao_btc}'''
-
-    texto_cotacoes["text"] = texto
+janela.close()
 
 
 
-janela = Tk()
-janela.title("Cotaçao Atual das Moedas")
 
-texto_orientacao = Label(janela, text="Clique no botao para exibir as cotaçoes das moedas Dólar/Euro/BTC")
-texto_orientacao.grid(column=0, row=0)
 
-botao = Button(janela, text="Buscar Cotações", command=pegarCotacoes)
-botao.grid(column=0, row=1)
 
-texto_cotacoes = Label(janela, text="")
-texto_cotacoes.grid(column=0, row=2)
-
-janela.mainloop()
